c6408fe554a114c5239c0540d0fde68ed8f6501e
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2023-01-11 00:16:56 +0300

    6.6.1 + insert

--- 6.6.1/6.6.1.cpp
+++ 6.6.1/6.6.1.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 
-#include <Source.cpp>
+#include <Classes.h>
 
 #include <Wt/Dbo/Dbo.h>
 #include <Wt/Dbo/backend/Postgres.h>
@@ -11,8 +11,8 @@ int main()
         std::string connectionString =
             "host=localhost"
             " port=5432"
-            " dbname= "
-            " user= "
+            " dbname="
+            " user="
             " password=";
 
         auto postgres = std::make_unique<Wt::Dbo::backend::Postgres>(connectionString);
@@ -26,7 +26,43 @@ int main()
         session.mapClass<Sale>("Sale");
 
         session.createTables();
+        
+        Wt::Dbo::Transaction transaction{session};
 
+        std::unique_ptr<Publisher> publishers{ new Publisher() };
+        publishers->name = "Star";
+        Wt::Dbo::ptr<Publisher> p = session.add(std::move(publishers));
+        
+        
+        std::unique_ptr<Book> books{ new Book() };
+        books->title = "Developer in C++";
+        Wt::Dbo::ptr<Publisher> name_pub = session.find<Publisher>().where("name = ?").bind("Star");
+        books->publisher = name_pub;
+        Wt::Dbo::ptr<Book> b = session.add(std::move(books));
+
+        std::unique_ptr<Shop> shops{ new Shop() };
+        shops->name = "Read the city";
+        Wt::Dbo::ptr<Shop> sh = session.add(std::move(shops));
+
+        
+        std::unique_ptr<Stock> stocks{ new Stock() };
+        stocks->count = 3;
+        Wt::Dbo::ptr<Book> title_book = session.find<Book>().where("title = ?").bind("Developer in C++");
+        stocks->book = title_book;
+        Wt::Dbo::ptr<Shop> name_shop = session.find<Shop>().where("name = ?").bind("Read the city");
+        stocks->shop = name_shop;
+        Wt::Dbo::ptr<Stock> st = session.add(std::move(stocks));
+
+        
+        std::unique_ptr<Sale> sales{ new Sale() };
+        sales->price = 35;
+        sales->data_sale = "15.01.2023";
+        sales->count = 5;
+        Wt::Dbo::ptr<Stock> id_stock = session.find<Stock>().where("id = ?").bind("1");
+        sales->stock = id_stock;
+        Wt::Dbo::ptr<Sale> sa = session.add(std::move(sales));
+
+        transaction.commit();
     }
     catch (const Wt::Dbo::Exception& e)
     {
